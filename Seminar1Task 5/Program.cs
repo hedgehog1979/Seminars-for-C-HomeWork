a572d5f9cd485c23c0f652053c830c15a8902043
hedgehog1979 <egor.pub@yandex.ru>
2022-08-08 09:45:31 +0400

    CopiedSeminar1Data

---
+++ Seminar1Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+
+if(count < N)
+{
+    while(count <= N)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+}
+else
+{
+    while(count >= N)
+    {
+        Console.WriteLine(N);
+        N++;
+    }
+}
---
+++ Seminar1Task 8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число от 2 и выше: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (N >= 1)
+    Console.WriteLine("Четные числа менее введенного числа: ");
+{
+    while (count <= N)
+    {
+        Console.WriteLine(count);
+        count = count + 2;
+    }
+}
