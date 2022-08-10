bcea4af5dd5b1b5db27122be4793b3d9c6f18a91
hedgehog1979 <egor.pub@yandex.ru>
2022-08-10 19:38:22 +0400

    Seminar 2 Homework uploaded

---
+++ Seminar2Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число и мы покажем Вам третью по порядку цифру в нем!");
+int num = Convert.ToInt32(Console.ReadLine());
+string NumStr = Convert.ToString(num);
+if (num >= 100 ^ num <= -100) 
+{
+    Console.WriteLine(NumStr[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры в числе не найдено");
+}
---
+++ Seminar2Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите порядковый номер дня недели, чтобы понять, случился ли выходной: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("суровые будни");
+        break;
+    
+    case 2:
+        Console.WriteLine("суровые будни");
+        break;
+    
+    case 3:
+        Console.WriteLine("на полпути");
+        break;
+   
+    case 4:
+        Console.WriteLine("суровые будни");
+        break;
+    
+    case 5:
+        Console.WriteLine("TGIF");
+        break;
+   
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+   
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+   
+    default:
+        Console.WriteLine("Введите существующий номер дня недели");
+        break;
+}
