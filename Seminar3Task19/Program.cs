3571009d7f1f79d6806128d002445396883682ba
hedgehog1979 <egor.pub@yandex.ru>
2022-08-15 17:29:29 +0400

    seminar3 homework added

---
+++ Seminar3Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число и мы посмотрим, является ли оно палиндромом");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+if (num >= 10000 & num <= 99999) 
+{
+    string number = num.ToString();
+    for (int i = 0; i < number.Length/2; i++)
+    {
+        if(number[i] != number[number.Length-1-i]) Console.WriteLine("Не палиндром");
+        else Console.WriteLine("Палиндром!");
+        break;
+    }  
+}
+else 
+{
+    Console.WriteLine("Пожалуйста, введите число из пяти цифр");
+}
---
+++ Seminar3Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double[] A = new double[3];
+double[] B = new double[3];
+double[] C = new double[3];
+Console.WriteLine("Введите точку x1: ");
+A[0] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку y1: ");
+B[0] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку z1: ");
+C[0] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку x2: ");
+A[1] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку Y2: ");
+B[1] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите точку z2: ");
+C[1] = Convert.ToDouble(Console.ReadLine());
+
+double Length = Math.Sqrt(Math.Pow(A[1]-A[0], 2) +Math.Pow(B[1]-B[0], 2) +Math.Pow(C[1]-C[0], 2));
+Console.WriteLine(Length);
